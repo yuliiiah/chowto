b5b95967ac6bd8ed080b9670ab950df0239eea4f
yuliiiah <horbenko.yuliya@gmail.com>
2019-07-12 23:29:00 +0300

    task2_21

---
+++ chapter2/homework/task2_21.c
@@ -0,0 +1,46 @@
+// Self-review exercises from Ch. 2: 2.21
+// Write a program that prints shapes with asterisks.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("*********\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*********\n");
+
+    printf("   ***   \n"
+           " *     * \n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           "*\t*\n"
+           " *     * \n"
+           "   ***   \n");
+
+    printf("    *  \n"
+           "   *** \n"
+           "  *****\n"
+           "    *  \n"
+           "    *  \n"
+           "    *  \n"
+           "    *  \n"
+           "    *  \n"
+           "    *  \n");
+
+    printf("    *    \n"
+           "   * *   \n"
+           "  *   *  \n"
+           " *     * \n"
+           "*       *\n"
+           " *     * \n"
+           "  *   *  \n"
+           "   * *   \n"
+           "    *    \n");
+}
