678a3ae0beab2437be8fb8b60663dd6594b617d6
yuliiiah <horbenko.yuliya@gmail.com>
2019-07-12 23:08:05 +0300

    task2_20

---
+++ chapter2/homework/task2_20.c
@@ -0,0 +1,21 @@
+// Self-review exercises from Ch. 2: 2.18
+// (Converting from seconds to hours, minutes and seconds) Write a program that asks the
+//  user to enter the total time elapsed, in seconds, since an event and converts the time to hours,
+//  minutes and seconds.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s", "Enter the total time in seconds: ");
+
+    int seconds, minutes, hours;
+    scanf("%d", &seconds);
+
+    minutes = seconds / 60;
+    seconds = seconds % 60;
+
+    hours = minutes / 60;
+    minutes = minutes % 60;
+
+    printf("%d:%d:%d\n", hours, minutes, seconds);
+}
