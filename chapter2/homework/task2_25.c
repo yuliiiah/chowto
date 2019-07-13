d0713bdc511f96e476cef0fb172865d238758512
yuliiiah <horbenko.yuliya@gmail.com>
2019-07-13 21:30:55 +0300

    task2_25

---
+++ chapter2/homework/task2_25.c
@@ -0,0 +1,17 @@
+// Self-review exercises from Ch. 2: 2.25
+// Print your initials in block letters down the page.
+// Construct each block letter out of the letter it represents.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("HHHHHHHHH\n"
+           "    H    \n"
+           "    H    \n"
+           "HHHHHHHHH\n");
+    printf("        Y\n"
+           "      Y  \n"
+           "YYYYY    \n"
+           "      Y  \n"
+           "        Y\n");
+}
