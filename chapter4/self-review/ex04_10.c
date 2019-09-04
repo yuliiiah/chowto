bab8e33d6e1d3920b9553a03aa74fdfa2719a37b
yuliiiah <horbenko.yuliya@gmail.com>
2019-09-04 23:23:11 +0300

    ex04_10

---
+++ chapter4/self-review/ex04_10.c
@@ -0,0 +1,16 @@
+// Chapter 4: Self-review exercise 4.9
+// (Conversion Celsius to Fahrenheit) Write a program that converts
+//  temperatures from 30 ° C to 50 ° C to the Fahrenheit scale.
+//  The program should print a table displaying temperatures in the
+//  two scales side by side. [Hint: ° F = 9/5C + 32]
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%-10s %10s", "Celcius", "Fahrenheit\n");
+
+    for (int celc = 30, fahr; celc <= 50; celc++) {
+        fahr = 1.8 * celc + 32;
+        printf("%-10d %-10d\n", celc, fahr);
+    }
+}
